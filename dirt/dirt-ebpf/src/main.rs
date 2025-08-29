13a53987504778c03d40caa44365e8a9d3433fde
bobbintb <bobbintb@gmail.com>
2025-08-28 21:26:12 -0600

    Update main.rs

--- dirt/dirt-ebpf/src/main.rs
+++ dirt/dirt-ebpf/src/main.rs
@@ -7,10 +7,10 @@ use aya_ebpf::{
     macros::{lsm, map},
     maps::{Array, PerfEventArray, PerCpuArray},
     programs::LsmContext,
-    helpers::{bpf_d_path, bpf_probe_read_kernel_str_bytes},
+    helpers::bpf_d_path,
 };
 use dirt_common::{EventType, UnlinkEvent};
-use vmlinux::{path, dentry};
+use vmlinux::path;
 
 const MAX_FILENAME_LEN: usize = 256;
 
@@ -32,43 +32,26 @@ pub fn lsm_path_unlink(ctx: LsmContext) -> i32 {
 }
 
 fn try_lsm_path_unlink(ctx: LsmContext) -> Result<i32, i32> {
-    let dir_path_ptr: *const path = unsafe { ctx.arg(0) };
-    if dir_path_ptr.is_null() {
+    let path_ptr: *const path = unsafe { ctx.arg(0) };
+    if path_ptr.is_null() {
         return Ok(0);
     }
-    let dir_path = unsafe { &*dir_path_ptr };
-
-    let dentry_ptr: *const dentry = unsafe { ctx.arg(1) };
-    if dentry_ptr.is_null() {
-        return Ok(0);
-    }
-    let dentry = unsafe { &*dentry_ptr };
+    let path = unsafe { &*path_ptr };
 
     let filename_buf = unsafe {
         let buf_ptr = FILENAME_BUF.get_ptr_mut(0).ok_or(-1)?;
         &mut *buf_ptr
     };
 
-    let dir_len = unsafe {
+    let path_str_len = unsafe {
         bpf_d_path(
-            dir_path as *const _ as *mut _,
+            path as *const _ as *mut _,
             filename_buf.as_mut_ptr() as *mut i8,
             MAX_FILENAME_LEN as u32,
         )
     };
 
-    if dir_len > 0 {
-        let dir_len = dir_len as usize;
-        if dir_len < MAX_FILENAME_LEN {
-            filename_buf[dir_len - 1] = b'/';
-            let _ = unsafe {
-                bpf_probe_read_kernel_str_bytes(
-                    dentry.d_name.name as *const _,
-                    &mut filename_buf[dir_len..],
-                )
-            };
-        }
-
+    if path_str_len > 0 {
         let pid_tgid = aya_ebpf::helpers::bpf_get_current_pid_tgid();
         let tgid = (pid_tgid >> 32) as u32;
 
