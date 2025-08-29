7493d0d1ef779a3ca558e5d7abad7b9a0b5cbfb1
bobbintb <bobbintb@gmail.com>
2025-08-29 02:56:04 -0600

    Update main.rs

--- dirt/dirt-ebpf/src/main.rs
+++ dirt/dirt-ebpf/src/main.rs
@@ -7,12 +7,14 @@ use aya_ebpf::{
     macros::{lsm, map},
     maps::{Array, PerfEventArray, PerCpuArray},
     programs::LsmContext,
-    helpers::bpf_d_path,
+    helpers::{bpf_d_path, bpf_get_current_pid_tgid, bpf_probe_read_kernel_str_bytes},
 };
 use dirt_common::{EventType, UnlinkEvent};
+use core::cmp;
 use vmlinux::path;
 
 const MAX_FILENAME_LEN: usize = 256;
+const MAX_PATH_LEN: usize = 4096;
 
 #[map]
 static TARGET_DEV: Array<u32> = Array::with_max_entries(1, 0);
@@ -21,7 +23,13 @@ static TARGET_DEV: Array<u32> = Array::with_max_entries(1, 0);
 static EVENTS: PerfEventArray<UnlinkEvent> = PerfEventArray::new(0);
 
 #[map]
-static FILENAME_BUF: PerCpuArray<[u8; MAX_FILENAME_LEN]> = PerCpuArray::with_max_entries(1, 0);
+static PATH_NAME_BUF: PerCpuArray<[u8; MAX_PATH_LEN]> = PerCpuArray::with_max_entries(1, 0);
+
+#[map]
+static FILE_NAME_BUF: PerCpuArray<[u8; MAX_FILENAME_LEN]> = PerCpuArray::with_max_entries(1, 0);
+
+#[map]
+static EVENT_BUF: PerCpuArray<UnlinkEvent> = PerCpuArray::with_max_entries(1, 0);
 
 #[lsm(hook = "path_unlink")]
 pub fn lsm_path_unlink(ctx: LsmContext) -> i32 {
@@ -38,38 +46,77 @@ fn try_lsm_path_unlink(ctx: LsmContext) -> Result<i32, i32> {
     }
     let path = unsafe { &*path_ptr };
 
-    let filename_buf = unsafe {
-        let buf_ptr = FILENAME_BUF.get_ptr_mut(0).ok_or(-1)?;
-        &mut *buf_ptr
-    };
+    let pathname_buf = PATH_NAME_BUF.get_ptr_mut(0).ok_or(-1)?;
+    let filename_buf = FILE_NAME_BUF.get_ptr_mut(0).ok_or(-1)?;
+    let event_buf = EVENT_BUF.get_ptr_mut(0).ok_or(-1)?;
 
-    let path_str_len = unsafe {
+    let path_len = unsafe {
         bpf_d_path(
             path as *const _ as *mut _,
-            filename_buf.as_mut_ptr() as *mut i8,
-            MAX_FILENAME_LEN as u32,
+            (&mut *pathname_buf)[..].as_mut_ptr() as *mut i8,
+            MAX_PATH_LEN as u32,
         )
     };
+    if path_len <= 0 {
+        return Ok(0);
+    }
 
-    if path_str_len > 0 {
-        let pid_tgid = aya_ebpf::helpers::bpf_get_current_pid_tgid();
-        let tgid = (pid_tgid >> 32) as u32;
+    let dentry_ptr: *const vmlinux::dentry = unsafe { ctx.arg(1) };
+    if !dentry_ptr.is_null() {
+        let dentry = unsafe { &*dentry_ptr };
+        let copy_len = cmp::min(
+            unsafe { dentry.d_name.__bindgen_anon_1.__bindgen_anon_1.len as usize },
+            MAX_FILENAME_LEN - 1,
+        );
+        unsafe {
+            let buf_ptr = (*filename_buf).as_mut_ptr();
+            let _ = bpf_probe_read_kernel_str_bytes(
+                dentry.d_name.name as *const u8,
+                core::slice::from_raw_parts_mut(buf_ptr, copy_len),
+            );
+        }
+    } else {
+        unsafe { (*filename_buf)[0] = 0; }
+    }
 
-        let target_dev = match TARGET_DEV.get(0) {
-            Some(val) => *val,
-            None => return Err(-1),
-        };
+    let pid_tgid = bpf_get_current_pid_tgid();
+    let tgid = (pid_tgid >> 32) as u32;
+    let pid = pid_tgid as u32;
 
-        let pid = pid_tgid as u32;
-        let event = UnlinkEvent {
-            event_type: EventType::FEntry,
-            pid,
-            tgid,
-            target_dev,
-            ret_val: 0,
-            filename: *filename_buf,
-        };
-        EVENTS.output(&ctx, &event, 0);
+    let target_dev = match TARGET_DEV.get(0) {
+        Some(val) => *val,
+        None => return Err(-1),
+    };
+
+    unsafe {
+        (*event_buf).event_type = EventType::FEntry;
+        (*event_buf).pid = pid;
+        (*event_buf).tgid = tgid;
+        (*event_buf).target_dev = target_dev;
+        (*event_buf).ret_val = 0i32;
+
+        let path_copy_len = cmp::min(path_len as usize, MAX_PATH_LEN - 1);
+        core::ptr::copy_nonoverlapping(
+            (&*pathname_buf)[..].as_ptr(),
+            (&mut (*event_buf).pathname)[..].as_mut_ptr(),
+            path_copy_len,
+        );
+        (*event_buf).pathname[path_copy_len] = 0;
+
+        let mut i = 0usize;
+        while i < MAX_FILENAME_LEN && (*filename_buf)[i] != 0 {
+            i += 1;
+        }
+        let fname_len = i;
+        let fn_copy = cmp::min(fname_len, MAX_FILENAME_LEN - 1);
+        core::ptr::copy_nonoverlapping(
+            (&*filename_buf)[..].as_ptr(),
+            (&mut (*event_buf).filename)[..].as_mut_ptr(),
+            fn_copy,
+        );
+        (*event_buf).filename[fn_copy] = 0;
+
+        EVENTS.output(&ctx, &*event_buf, 0);
     }
 
     Ok(0)
